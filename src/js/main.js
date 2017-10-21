b8f13b6d5cf91ade483bad7c7dc2a838bbabb1d2
Benjamin Naesen <bnaesen@gmail.com>
2017-10-21 19:12:40 +0200

    Date Created + Rows

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,13 @@ document.getElementById('myForm').addEventListener('submit', (e) => {
     // Receive form values
     var siteName = siteNameInput.value;
     var siteURL = siteURLInput.value;
+    var currentDate = new Date();
+    var theDate = currentDate.getDate() + "/" + (currentDate.getMonth() + 1) + "/" + currentDate.getFullYear();
     var boolean = true;
     var bookmark = {
         name: siteName,
-        url: siteURL
+        url: siteURL,
+        date: theDate
     }
     var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
     var regex = new RegExp(expression);
@@ -25,7 +28,7 @@ document.getElementById('myForm').addEventListener('submit', (e) => {
         siteURLInput.style.borderColor = 'red';
         boolean = false;
     }
-    
+
     if (!siteURL.match(regex)) {
         siteURLInput.style.borderColor = 'red';
         clearInput(siteURLInput);
@@ -89,7 +92,7 @@ function colorGrey() {
     siteURLInput.style.borderColor = '#D3D3D3';
 }
 
-function clearInput(siteInput){
+function clearInput(siteInput) {
     siteInput.value = '';
 }
 
@@ -107,13 +110,18 @@ function fetchBookmarks() {
     for (var i = 0; i < bookmarks.length; i++) {
         var name = bookmarks[i].name;
         var url = bookmarks[i].url;
+        var date = bookmarks[i].date;
+        var hr = '';
 
-        divBookmarks.innerHTML += '<div class="alert alert-secondary" role="alert">' +
-            '<h4>' + name +
-            '<a class="btn btn-default" target="_blank" href="' + url + '">Visit</a>' +
-            '<a onclick="deleteBookmark(\'' + url + '\')" class="btn btn-danger" href="#">Delete</a>' +
-            '</h4>' +
-            '</div>';
+        if (i != bookmarks.length - 1) {
+            hr = '<hr />';
+        }
+
+        divBookmarks.innerHTML += '<div class="row"><div class="col col-md-8">' +
+            '<strong>' + name + '</strong></div>' +
+            '<div class="col col-md-1"><a  target="_blank" href="' + url + '">Visit</a></div>' +
+            '<div class="col col-md-1 red"><a onclick="deleteBookmark(\'' + url + '\')" href="#">Delete</a></div>' +
+            '<div class="col col-md-2">' + date + '</div></div>' + hr;
     }
 
 }
